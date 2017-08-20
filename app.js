ab36cd2bb70b4f0f78ca0a27d84df64cf6b6a335
Andreas Stenlund <a.stenlund@gmail.com>
2017-08-20 15:48:04 +0200

    New route: /mobidoc/version
    Returns the package version.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 /* jshint esversion: 6 */
 
 const express = require('express');
+const pkg     = require('./package');
+const sprintf = require('sprintf-js').sprintf;
 
 const app = express();
+
+app.get('/mobidoc/version', (req, res) => {
+    console.log(sprintf('\n%s -> %s', req.hostname, req.url));
+    res.send(pkg.version);
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
